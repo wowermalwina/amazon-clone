625e6579dd3e902f397f712299f36bd9e1abb12a
wowermalwina <wowermalwina@gmail.com>
2022-01-16 14:27:06 +0000

    created Product component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import "./Home.scss";
+import homeImage from "../../assets/images/amazon_background.jpg";
+import Product from "../Product/Product";
 
 const Home = () => {
   return (
     <div className="home">
-      <div className="home__container"></div>
+      <div className="home__container">
+        <img className="home__image" src={homeImage} alt="home image" />
+      </div>
+      <div className="home__row">
+        <Product />
+        <Product />
+      </div>
+      <div className="home__row"></div>
     </div>
   );
 };
