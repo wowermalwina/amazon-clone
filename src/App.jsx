6cafbf074ec2df0fce775a10667c4ca31378d8c8
wowermalwina <wowermalwina@gmail.com>
2022-01-18 23:13:14 +0000

    created checkout component and started styling it

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import Checkout from "./components/Checkout/Checkout";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -10,7 +11,7 @@ const App = () => {
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/checkout" element={<h1>I AM A CHECKOUT</h1>} />
+          <Route path="/checkout" element={<Checkout />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
---
+++ src/components/Checkout/Checkout.jsx
@@ -0,0 +1,17 @@
+import "./Checkout.scss";
+
+const Checkout = () => {
+  return (
+    <div className="checkout">
+      <div className="checkout__left"></div>
+      <img
+        className="checkout__ad"
+        src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+        alt=""
+      />
+      <div className="checkout__right"></div>
+    </div>
+  );
+};
+
+export default Checkout;
