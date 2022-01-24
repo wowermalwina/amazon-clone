7bb8842a9001b485fb06e0270b001896e1fa2f7d
wowermalwina <wowermalwina@gmail.com>
2022-01-24 22:11:58 +0000

    working on styling

--- src/components/CheckoutProduct/CheckoutProduct.jsx
+++ src/components/CheckoutProduct/CheckoutProduct.jsx
@@ -6,12 +6,12 @@ const CheckoutProduct = ({ id, image, title, price, rating }) => {
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} />
       <div classname="checkoutProduct__info">
-        <p className="checkoutProduct__title">{title}</p>
-        <p className="checkoutProduct__price">
+        <p className="checkoutProduct__info__title">{title}</p>
+        <p className="checkoutProduct__info__price">
           <small>£</small>
           <strong>{price}</strong>
         </p>
-        <div className="checkoutProduct__rating">
+        <div className="checkoutProduct__info__rating">
           {Array(rating)
             .fill()
             .map(() => (
