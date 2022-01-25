41ade16d19b742b0fe0f87192713691a62d8995a
wowermalwina <wowermalwina@gmail.com>
2022-01-25 22:30:48 +0000

    updated reducer

--- src/components/CheckoutProduct/CheckoutProduct.jsx
+++ src/components/CheckoutProduct/CheckoutProduct.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import "./CheckoutProduct.scss";
+import { useStateValue } from "../../StateProvider";
 
-const CheckoutProduct = ({ id, image, title, price, rating }) => {
+const CheckoutProduct = ({ id, image, title, price, rating, hideButton }) => {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const removeFromBasket = () => {
+    // remove the item from the basket
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  };
+
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} />
@@ -18,7 +29,9 @@ const CheckoutProduct = ({ id, image, title, price, rating }) => {
               <p>⭐</p>
             ))}
         </div>
-        <button>Remove from Basket</button>
+        {!hideButton && (
+          <button onClick={removeFromBasket}>Remove from Basket</button>
+        )}{" "}
       </div>
     </div>
   );
