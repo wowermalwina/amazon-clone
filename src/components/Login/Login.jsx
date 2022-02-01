83a8e3f8048eeaf007f7ca67b921cbbb7badb373
wowermalwina <wowermalwina@gmail.com>
2022-02-01 22:50:31 +0000

    working on login functionality

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.scss";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (event) => {
+    event.preventDefault();
+  };
+  const register = (event) => {
+    event.preventDefault;
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -15,17 +25,31 @@ const Login = () => {
         <h1>Sign in</h1>
         <form>
           <h5>E-mail</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
           <h5>Password </h5>
-          <input type="password" />
-          <button className="login__signInButton">Sign In</button>
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <button
+            type="submit"
+            className="login__signInButton"
+            onClick={signIn}
+          >
+            Sign In
+          </button>
         </form>
         <p>
           By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
           Sale. Please see our Privacy Notice, our Cookies Notice and our
           Inerest-Based Ads Notice.
         </p>
-        <button className="login__registerButton">
+        <button className="login__registerButton" onCLick={register}>
           Create your amazon account
         </button>
       </div>
