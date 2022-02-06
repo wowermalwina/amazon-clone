254cf2f262b7c8e656fc09bb109dbe785240bd57
wowermalwina <wowermalwina@gmail.com>
2022-02-06 21:53:09 +0000

    added firebase and auth;

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDPNCNV89pDP9TJ5f1_kxr0aaC_sTLEJjI",
   authDomain: "clone-9be55.firebaseapp.com",
@@ -8,3 +10,12 @@ const firebaseConfig = {
   appId: "1:705459318793:web:50a1460b076874339088d8",
   measurementId: "G-315SCT1WRS",
 };
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+// const analytics = getAnalytics(firebaseApp);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
