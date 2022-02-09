3eeb767ed1d8843b1ab9f2268712cb1ff872fc8e
wowermalwina <wowermalwina@gmail.com>
2022-02-09 22:12:16 +0000

    working on checkout, added firebase tools

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -4,26 +4,36 @@ import CheckoutProduct from "../CheckoutProduct/CheckoutProduct";
 import { useStateValue } from "../../StateProvider";
 
 const Checkout = () => {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
+
   return (
     <div className="checkout">
       <div className="checkout__left">
         <img
           className="checkout__ad"
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
-          alt="checkout ad"
+          alt=""
         />
-        <h2 className="checkout__title">Your shopping basket</h2>
-        {basket.map((item) => (
-          <CheckoutProduct
-            id={item.id}
-            title={item.title}
-            image={item.image}
-            price={item.price}
-            rating={item.rating}
-          />
-        ))}
+
+        <div>
+          <h3>
+            Hello,
+            {user ? user.email : " User"}
+          </h3>
+          <h2 className="checkout__title">Your shopping Basket</h2>
+
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
+        </div>
       </div>
+
       <div className="checkout__right">
         <Subtotal />
       </div>
